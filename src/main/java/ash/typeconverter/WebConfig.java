1fc25541c5407d7548049109cf868cff6786e91c
Ahram-Jeong <ahram1215@gmail.com>
2024-01-25 17:29:34 +0900

    20240125 - Formatter 적용

--- src/main/java/ash/typeconverter/WebConfig.java
+++ src/main/java/ash/typeconverter/WebConfig.java
@@ -1,9 +1,8 @@
 package ash.typeconverter;
 
-import ash.typeconverter.converter.IntegerToStringConverter;
 import ash.typeconverter.converter.IpPortToStringConverter;
-import ash.typeconverter.converter.StringToIntegerConverter;
 import ash.typeconverter.converter.StringToIpPortConverter;
+import ash.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 우선 순위로 인한 주석 처리
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
