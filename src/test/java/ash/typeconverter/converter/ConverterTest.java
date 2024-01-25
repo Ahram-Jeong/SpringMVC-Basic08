d8414f9be056a133c951f2b9a7713c1df29d7118
Ahram-Jeong <ahram1215@gmail.com>
2024-01-25 11:21:06 +0900

    20240125 - 타입 컨버터 (Converter)

---
+++ src/test/java/ash/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,38 @@
+package ash.typeconverter.converter;
+
+import ash.typeconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("99");
+        assertThat(result).isEqualTo(99);
+    }
+
+    @Test
+    void IntegerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(85);
+        assertThat(result).isEqualTo("85");
+    }
+    
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}
