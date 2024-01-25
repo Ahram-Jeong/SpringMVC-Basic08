0b846f233d24997d7b693ef83933c82b702b70d7
Ahram-Jeong <ahram1215@gmail.com>
2024-01-25 17:21:23 +0900

    20240125 - Formatter를 지원하는 ConversionService

---
+++ src/test/java/ash/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,30 @@
+package ash.typeconverter.formatter;
+
+import ash.typeconverter.converter.IpPortToStringConverter;
+import ash.typeconverter.converter.StringToIpPortConverter;
+import ash.typeconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FormattingConversionServiceTest {
+    @Test
+    void formattingConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        // Converter 등록
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // Formatter 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        // Converter 사용
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        // Formatter 사용
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
+    }
+}
